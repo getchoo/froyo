8c153d378dd249cfc17c38c5d895542c3b743673
Seth Flynn <getchoo@tuta.io>
2025-10-12 23:22:00 -0400

    flake: add `lib.toStandardOutputs`

--- flake.nix
+++ flake.nix
@@ -1,5 +1,33 @@
 {
-  outputs = _: {
-    lib.new = import ./default.nix;
-  };
+  outputs =
+    _:
+
+    let
+      toStandardOutputs =
+        froyo:
+        {
+          inherit froyo;
+          /*
+            FIXME: This should work and be exported!
+
+            The current implementation will properly export the extended configuration as `froyo` above,
+            but the following `outputs` and `perTarget` attributes remain untouched.
+
+            Why???? I have no idea. Maybe Nix lang is doing some caching nonsense? -getchoo
+          */
+          # extend = module: toStandardOutputs (froyo.extend module);
+        }
+        // builtins.removeAttrs froyo.outputs [ "perTarget" ]
+        // builtins.foldl' (
+          acc: system:
+          acc // builtins.mapAttrs (_: value: { ${system} = value; }) froyo.outputs.perTarget.${system}
+        ) { } (builtins.attrNames froyo.outputs.perTarget);
+    in
+
+    {
+      lib = {
+        new = import ./default.nix;
+        inherit toStandardOutputs;
+      };
+    };
 }
